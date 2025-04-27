38d8e5a922c875ae2e35825cb7dbaef00b71a040
unknown <nurfauzymochriyadhy@gmail.com>
2025-04-27 22:11:39 +0700

    menambahkan bilangan genap

---
+++ genap.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>genap</title>
+</head>
+<body>
+<?php
+    echo "Bilangan genap dari 1-10 : ";
+    for ($i = 1; $i <= 10; $i++) {
+     if ($i % 2 == 0) {
+        echo $i . " ";
+     }
+    }
+    ?>
+</body>
+</html>
